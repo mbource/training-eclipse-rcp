2ab7a8e3b9cb89ca3da3614dd8bb6b7aeb0c8ee4
Matthieu Bource <matthieu.bource@bonitasoft.com>
2022-03-22 14:58:42 +0100

    Adding colors to treeviewer

--- com.bonita.rental.ui/src/com/bonita/rental/ui/providers/RentalProvider.java
+++ com.bonita.rental.ui/src/com/bonita/rental/ui/providers/RentalProvider.java
@@ -3,14 +3,42 @@ package com.bonita.rental.ui.providers;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
 
-public class RentalProvider extends LabelProvider implements ITreeContentProvider {
+public class RentalProvider extends LabelProvider implements ITreeContentProvider, IColorProvider {
+
+	@Override
+	public Color getForeground(Object element) {
+		if (element != null && element instanceof RentalObject) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
+		}
+
+		if (element != null && element instanceof Customer) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
+		}
+
+		if (element != null && element instanceof Rental) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_GREEN);
+		}
+
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public Object[] getElements(Object inputElement) {
